0a6aa92269a1a9d7543cd4f52fde589f9438479d
mugarte-lab <v_mugarte@ufms.br>
2022-07-02 21:10:14 -0400

    alteração dia 02/07

--- app/src/main/java/com/example/trabalhofinalprogmobile/Login.java
+++ app/src/main/java/com/example/trabalhofinalprogmobile/Login.java
@@ -2,13 +2,34 @@ package com.example.trabalhofinalprogmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        getSupportActionBar().hide();
+        IniciarComponentes();
+
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Login.this, Cadastro.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void IniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.tela_cadastro);
     }
 }
--- app/src/main/java/com/example/trabalhofinalprogmobile/MainActivity.java
+++ app/src/main/java/com/example/trabalhofinalprogmobile/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.trabalhofinalprogmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text_tela_inicial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        IniciarComponentes();
+
+        text_tela_inicial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void IniciarComponentes(){
+        text_tela_inicial = findViewById(R.id.botao_tela_inicial);
     }
 }
